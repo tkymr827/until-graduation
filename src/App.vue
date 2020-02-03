099ab94d6562aeb7f5aa12b7c5a4e22628a72bf8
tkym <lelele5@RyoyanoMacBook-Air.local>
2020-02-03 11:11:29 +0900

    docs

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,53 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <countdown1 :day="day" :hour="hour" :min="min" :sec="sec"></countdown1>
+
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import countdown1 from './components/countdown1.vue'
 export default {
-  name: 'app',
   components: {
-    HelloWorld
+    countdown1
+  },
+  data () {
+    return {
+      day: null,
+      hour: null,
+      min: null,
+      sec: null
+    }
+  },
+  mounted () {
+    setInterval(() => {
+      const graduation = new Date(2020, 2, 4, 14, 25)
+      const now = new Date()
+      let until = graduation - now
+
+      this.day = Math.floor(until / (1000 * 60 * 60 * 24))
+      until -= (this.day * (1000 * 60 * 60 * 24))
+
+      this.hour = Math.floor(until / (1000 * 60 * 60))
+      until -= (this.hour * (1000 * 60 * 60))
+
+      this.min = Math.floor(until / (1000 * 60))
+      until -= (this.min * (1000 * 60))
+
+      this.sec = Math.floor(until / 1000)
+    }, 1000)
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+    padding:0;
+    margin:0;
+    box-sizing: border-box;
 }
+html{
+    font-size:62.5%;
+}
+
 </style>
